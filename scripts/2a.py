09d3366e32b1ef88b4e73e031c9b5e48fc44d674
rer3378 <rer3378@rit.edu>
2019-04-03 14:54:04 -0400

    2a done, figures made

--- scripts/2a.py
+++ scripts/2a.py
@@ -1,13 +1,20 @@
 import sympy
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
 
-def solvel2(R_val,l1_val,K_val):
+def solvel2sym(R_val,l1_val,K_val):
     l2 = sympy.symbols('l2')
     equation = (l1_val*l2) - K_val*((l1_val + l2)**2)
     l2_vals = sympy.solve([sympy.Eq(equation, R_val)], [l2])
     return l2_vals
+
+def solvel2(R_val,l1_val,K_val):
+    num1 = -np.sqrt(l1_val**2 - 4*K_val*(R + l1_val**2)) - 2*K_val*l1_val + l1_val
+    ans1 = num1 / (2 * K_val)
+    num2 = np.sqrt(l1_val**2 - 4*K_val*(R + l1_val**2)) - 2*K_val*l1_val + l1_val
+    ans2 = num2 / (2 * K_val)
+    return ans1, ans2
+
 
 def getl2(R_val, l1_vals, K_val):
     pos_arr, neg_arr = [], []
@@ -20,20 +27,22 @@ def getl2(R_val, l1_vals, K_val):
 
 l1_vals = np.arange(0.0, 50.0, 0.5)
 
-R_vals = [0.5, 10, 40]
-K_vals = [0.05] * 3
+R_vals = [10, 100, 800]
+K_vals = [0.05] * len(R_vals)
 
 for (R, K) in zip(R_vals, K_vals):
     pos_arr, neg_arr = getl2(R, l1_vals, K)
 
-    plt.scatter(l1_vals, neg_arr)
-    plt.title("R = " + str(R) + ", K = " + str(K))
+    plt.plot(l1_vals, neg_arr, linewidth=5)
+    plt.title("R = {}, K = {}".format(R,K))
     plt.xlabel('Lambda1')
     plt.xlim(0,50)
     plt.ylabel('Lambda2')
     plt.ylim(0,50)
     plt.grid()
+    plt.ion()
     plt.show()
     ans = input("Good fig?")
     if ans == 'y' or ans == 'Y':
-        plt.savefig('R{}K{}.png'.format(R, K))
+        plt.savefig('../figures/' + 'R{}K{}'.format(R, K).replace('.', '_') + '.png')
+    plt.close()
